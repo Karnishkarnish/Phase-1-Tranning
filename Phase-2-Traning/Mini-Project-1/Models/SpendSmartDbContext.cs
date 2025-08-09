49571f58f6061dd86a4028666490f4e73b53af99
karnish N <karnishkarnish300@gmail.com>
2025-08-09 16:21:20 +0530

    Add files via upload

---
+++ Phase-2-Traning/Mini-Project-1/Models/SpendSmartDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public DbSet<Expence> Expences { get; set; }
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options) : base(options) { }
+    }
+}
